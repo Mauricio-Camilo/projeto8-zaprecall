fb17017ee420b6411adf7a93d03a8aede4cb3ce5
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-03-18 12:39:32 -0500

    icone de virar flashcard e mostrar resposta implementado

--- src/Componentes/Pergunta.js
+++ src/Componentes/Pergunta.js
@@ -2,7 +2,7 @@ function Pergunta (props) {
     // const [tela, setTela] = React.useState (true);
     return (
         <div class="perguntas">
-            <p>{props.questao}</p>
+            <p>{props.flashcard}</p>
             <ion-icon onClick={() => props.setAnswering(true)} class="perguntas-icone" name="play-outline"></ion-icon>
         </div>
     )
--- src/Componentes/RenderizarPerguntas.js
+++ src/Componentes/RenderizarPerguntas.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Pergunta from "./Pergunta";
-import FlashcardFrente from "./FlashcardFrente";
+import Flashcard from "./Flashcard";
 
 
 function RenderizarPerguntas(props) {
     const [answering, setAnswering] = React.useState(false);
+    const [cardVirado, setCardVirado] = React.useState(false);
 
     return (
         <>
-        {answering? <FlashcardFrente questao={props.pergunta}/> : <Pergunta questao={props.flashcard} setAnswering={setAnswering}/>}      
+        {answering? 
+        <Flashcard pergunta={props.pergunta} resposta={props.resposta} estado={cardVirado} cardVirado={setCardVirado}/> : 
+        <Pergunta flashcard={props.flashcard} setAnswering={setAnswering}/>}      
         </> 
     )
 }
